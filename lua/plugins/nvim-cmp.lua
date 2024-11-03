f24422b8806931451fd7074fa4aa821b0e3e8c72
efettf <efettf@proton.me>
2024-11-03 21:39:49 +0100

    :heavy_plus_sign: Move cmp sources

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -3,14 +3,16 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-      'hrsh7th/cmp-buffer',
-      'efettf/cmp-emoji.nvim',
+      "hrsh7th/cmp-path",
+      "ray-x/cmp-treesitter",
+      "efettf/cmp-emoji.nvim",
     },
     config = function()
       require("cmp").setup({
         sources = require("cmp").config.sources({
-          { name = "buffer" },
           { name = "emoji", option = { insert = false } },
+          { name = "path" },
+          { name = "treesitter" },
         }),
         mapping = require("cmp").mapping.preset.insert({
           ['<down>'] = require('cmp').mapping.select_next_item(),
