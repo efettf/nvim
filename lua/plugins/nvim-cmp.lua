dee3ade1597df5410d7a7028501a9c117420c2e7
efettf <efettf@proton.me>
2024-11-03 22:42:11 +0100

    :heavy_plus_sign: Add cmp spell source

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -4,6 +4,7 @@ return {
     event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-path",
+      "f3fora/cmp-spell",
       "ray-x/cmp-treesitter",
       "efettf/cmp-emoji.nvim",
     },
@@ -12,7 +13,8 @@ return {
         sources = require("cmp").config.sources({
           { name = "emoji", option = { insert = false } },
           { name = "path" },
-          { name = "treesitter" },
+          { name = "treesitter", keyword_length = 2 },
+          { name = "spell", keyword_length = 4 },
         }),
         mapping = require("cmp").mapping.preset.insert({
           ['<down>'] = require('cmp').mapping.select_next_item(),
