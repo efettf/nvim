f360a6271573750117b748ffa4ef664759982427
efettf <efettf@proton.me>
2024-11-04 20:48:27 +0100

    :art: Prefix vim options

--- init.lua
+++ init.lua
@@ -7,44 +7,49 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-vim.g.neovide_transparency = 0.85
-vim.g.neovide_padding_bottom = 10
-vim.g.neovide_padding_right = 7
-vim.g.neovide_padding_left = 1
-vim.g.neovide_padding_top = 10
-vim.g.neovide_scale_factor = 0.8
+local o = vim.o
+local g = vim.g
+local a = vim.api
+local k = vim.keymap
+
+g.mapleader = " "
+g.neovide_transparency = 0.85
+g.neovide_padding_bottom = 10
+g.neovide_padding_right = 7
+g.neovide_padding_left = 1
+g.neovide_padding_top = 10
+g.neovide_scale_factor = 0.8
 
 require("lazy").setup({
   spec = { import = "plugins" }
 })
 
-vim.keymap.set("n", "<leader>r", "<cmd>Lazy<cr>")
+k.set("n", "<leader>r", "<cmd>Lazy<cr>")
 
-vim.api.nvim_set_keymap("n", "<c-=>", "<cmd>lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<cr>", { silent = true })
-vim.api.nvim_set_keymap("n", "<c-->", "<cmd>lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<cr>", { silent = true })
+a.nvim_set_keymap("n", "<c-=>", "<cmd>lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<cr>", { silent = true })
+a.nvim_set_keymap("n", "<c-->", "<cmd>lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<cr>", { silent = true })
 
-vim.o.autoread = true
-vim.o.backup = false
-vim.o.clipboard = "unnamedplus"
-vim.o.expandtab = true
-vim.o.foldlevel = 6
-vim.o.formatoptions = "cr"
-vim.o.guicursor = "a:hor20"
-vim.o.ignorecase = true
-vim.o.incsearch = true
-vim.o.linespace = 2
-vim.o.magic = true
-vim.o.number = true
-vim.o.scrolloff = 8
-vim.o.shiftwidth = 2
-vim.o.smartcase = true
-vim.o.smarttab = true
-vim.o.splitbelow = true
-vim.o.splitright = true
-vim.o.swapfile = false
-vim.o.tabstop = 2
-vim.o.termguicolors = true
-vim.o.writebackup = false
+o.autoread = true
+o.backup = false
+o.clipboard = "unnamedplus"
+o.expandtab = true
+o.foldlevel = 6
+o.formatoptions = "cr"
+o.guicursor = "a:hor20"
+o.ignorecase = true
+o.incsearch = true
+o.linespace = 2
+o.magic = true
+o.number = true
+o.scrolloff = 8
+o.shiftwidth = 2
+o.smartcase = true
+o.smarttab = true
+o.splitbelow = true
+o.splitright = true
+o.swapfile = false
+o.tabstop = 2
+o.termguicolors = true
+o.writebackup = false
 
 
