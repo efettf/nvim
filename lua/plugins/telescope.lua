458524ca0ae3b03d2e37f9b56ec5eb22d27607ff
efettf <efettf@proton.me>
2024-11-05 18:05:06 +0100

    :wrench: Add telescope key

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
     {
       'nvim-telescope/telescope.nvim',
       dependencies = { 'nvim-lua/plenary.nvim' },
+      cmd = "Telescope",
       keys = {
         { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Telescope files" },
         { "<leader>n", "<cmd>Telescope live_grep<cr>", desc = "Telescope grep" },
